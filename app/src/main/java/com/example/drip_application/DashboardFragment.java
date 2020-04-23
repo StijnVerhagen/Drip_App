c59ca7b8860697935f3f212ecaa070e18958d084
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2020-04-23 21:10:41 +0200

    Button onclick in fragment
    Zonder succes :(

--- app/src/main/java/com/example/drip_application/DashboardFragment.java
+++ app/src/main/java/com/example/drip_application/DashboardFragment.java
@@ -1,20 +1,50 @@
 package com.example.drip_application;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-    public class DashboardFragment extends Fragment {
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_dashboard, container, false);
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-            String stijn;
-        }
+public class DashboardFragment extends Fragment implements View.OnClickListener {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View dashboardView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final Button subscribeButton = (Button)dashboardView.findViewById(R.id.butSubcribe);
+        Log.i("DASHBOARDFRAG", "Yayyyy");
+        subscribeButton.setOnClickListener(this);
+        return dashboardView;
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        Log.i("DASHBOARDFRAG", "Jeuj");
+        Toast.makeText(getActivity(), "Hello World", Toast.LENGTH_LONG).show();
+    }
+}
+
+
+
+
+
+
+
--- app/src/main/java/com/example/drip_application/MainActivity.java
+++ app/src/main/java/com/example/drip_application/MainActivity.java
@@ -10,7 +10,10 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
